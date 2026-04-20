b159ddfa671b2692ef9ce0aa9253f1fe4dd4a32d
Suhail Surtee <surteesuhail@outlook.com>
2026-04-20 15:03:35 +0200

    V0.4

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import webhookRoutes from './src/modules/webhooks/webhook.routes.js';
 import adminRoutes from './src/modules/admin/admin.routes.js';
 import paystackRoutes from './src/modules/payments/paystack/paystack.routes.js';
 import { paystackWebhook } from './src/modules/payments/paystack/paystack.controller.js';
+import routes from './src/routes/index.js';
 
 const app = express();
 
@@ -29,6 +30,7 @@ app.use('/wallet/load/paystack', paystackRoutes);
 app.use('/webhooks', webhookRoutes);
 app.use('/admin', adminRoutes);
 app.use('/api', routes);
+app.use('/', routes);
 
 // Render requires dynamic port
 const PORT = process.env.PORT || 4000;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import authRoutes from '../modules/auth/auth.routes.js';
 import walletRoutes from '../modules/wallet/wallet.routes.js';
 import topupRoutes from '../modules/topups/topup.routes.js';
+import topupRoutes from '../modules/topup/topup.routes.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/wallet', walletRoutes);
+router.use('/admin', adminRoutes);
 router.use('/topup', topupRoutes);
 
 export default router;
